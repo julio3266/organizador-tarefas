a46008089d2f3b81ea39b44f7e2c95fb9c6f2444
Julio Valente <juliovalente321@gmail.com>
2020-01-31 15:11:13 -0300

    configurações de eventos e daygrid

---
+++ projeto-js/js/calendar.js
@@ -0,0 +1,10 @@
+function calendar()
+{
+  let calendarEl = document.getElementById('main');
+
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'dayGrid' ]
+  });
+
+  return calendar.render();
+}
